0d834fa68bac89ef003fe262a194b7cd97d7190c
Marine PAYET <marine.payet3@gmail.com>
2023-05-25 16:59:34 +0200

    wip

---
+++ src/app/sign-up/sign-up.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { UserComponent } from '../user/user.component';
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: './sign-up.component.html',
+  styleUrls: ['./sign-up.component.css']
+})
+export class SignUpComponent {
+
+  model: UserComponent = new UserComponent;
+
+  constructor() {}
+
+  onSubmit(){
+    console.log(this.model.email + ' ' + this.model.firstname + ' ' + this.model.lastname + ' ' + this.model.password);
+  }
+}
